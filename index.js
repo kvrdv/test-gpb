c6e8c0cd096108a407439140d5da19ee71f517e0
kvrdv <s.kovardaev@gmail.com>
2022-01-30 11:40:53 +0300

    correct sort & comments

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const path = require('path');
 const fs = require('fs');
 
 function getResult() {
+  // синхронно получаем входящие данные из файлов
   let input1;
   let input2;
+
   try {
     input1 = JSON.parse(fs.readFileSync('./data/input1.json', 'utf8'));
     input2 = JSON.parse(fs.readFileSync('./data//input2.json', 'utf8'));
@@ -11,15 +13,21 @@ function getResult() {
     console.log('Error reading file');
   }
 
+  // вычисляем длину самого короткого массива
+  const theLength = [input1.length, input2.length];
+  theLength.sort(function (a, b) {
+    return a - b;
+  });
+
+  // обрабатываем полученные данные в цикле
   let operationNumber = 0;
   let operationSuccess = 0;
   let failedInput1 = [];
   let failedInput2 = [];
   let output = [];
 
-  for (let i = 0; i < [input1.length, input2.length].sort()[0]; i++) {
+  for (let i = 0; i < theLength[0]; i++) {
     operationNumber++;
-
     if (input1[i] === undefined || input1[i] === null) {
       failedInput1.push(operationNumber);
       output.push({ failed: 'error in data 1' });
@@ -53,8 +61,8 @@ function getResult() {
     failedInput2: failedInput2,
   });
 
+  // записываем результат в файл
   const filePath = path.join(__dirname, 'data', 'output.json');
-
   fs.writeFile(filePath, JSON.stringify(output), function (err) {
     if (err) throw err;
   });
